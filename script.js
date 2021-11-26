555560bdad893518167da5e594e137e9e3f7b99f
manohar712 <87490669+manohar712@users.noreply.github.com>
2021-11-26 21:25:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ var per3 = new person("Krishna","Teja","Hyderabad");
 
 
 console.log("Person1:")
-console.log("FirstName: " + Person1.FirstName)
-console.log("LastName: " + Person1.LastName)
-console.log("OfficeAddress: " + Person1.OfficeAddress)
+console.log("FirstName: " + Per1.FirstName)
+console.log("LastName: " + Per1.LastName)
+console.log("OfficeAddress: " + Per1.OfficeAddress)
 
 console.log("Person2:")
-console.log("FirstName: " + Person2.FirstName)
-console.log("LastName: " + Person2.LastName)
-console.log("OfficeAddress: " + Person2.OfficeAddress)
+console.log("FirstName: " + Per2.FirstName)
+console.log("LastName: " + Per2.LastName)
+console.log("OfficeAddress: " + Per2.OfficeAddress)
 
 console.log("Person3:")
-console.log("FirstName: " + Person3.FirstName)
-console.log("LastName: " + Person3.LastName)
-console.log("OfficeAddress: " + Person3.OfficeAddress)
+console.log("FirstName: " + Per3.FirstName)
+console.log("LastName: " + Per3.LastName)
+console.log("OfficeAddress: " + Per3.OfficeAddress)
